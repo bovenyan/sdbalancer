1005272f964c72068b22e99edb55678a4972bd9e
Boven <boven.yan@gmail.com>
2017-10-21 14:54:55 -0400

    loadbalancer

--- lb1in2out.py
+++ lb1in2out.py
@@ -16,13 +16,15 @@ class MyTopo(Topo):
 
         H1 = self.addHost('h1', mac='00:00:00:00:00:01', IP='10.0.0.1')
         H2 = self.addHost('h2', mac='00:00:00:00:00:02', IP='10.0.0.2')
+        S0 = self.addSwitch('s0', protocols='OpenFlow13')
         S1 = self.addSwitch('s1', protocols='OpenFlow13')
         S2 = self.addSwitch('s2', protocols='OpenFlow13')
         S3 = self.addSwitch('s3', protocols='OpenFlow13')
         S4 = self.addSwitch('s4', protocols='OpenFlow13')
 
         # Add links
-        self.addLink(H1, S1, bw=100, cls=TCLink) 
+        self.addLink(H1, S0, bw=100, cls=TCLink) 
+        self.addLink(S0, S1, bw=100, cls=TCLink) 
         self.addLink(S1, S2, bw=50, cls=TCLink)
         self.addLink(S1, S3, bw=50, cls=TCLink)
         self.addLink(S2, S4, bw=50, cls=TCLink)
--- ovs_config.sh
+++ ovs_config.sh
@@ -1,14 +1,31 @@
-sudo ovs-vsctl set bridge s1 protocols=OpenFlow13
-sudo ovs-vsctl set bridge s2 protocols=OpenFlow13
-sudo ovs-vsctl set bridge s3 protocols=OpenFlow13
-sudo ovs-vsctl set bridge s4 protocols=OpenFlow13
-
+sudo ovs-ofctl del-flows s0 -O OpenFlow13
 sudo ovs-ofctl del-flows s1 -O OpenFlow13
+sudo ovs-ofctl del-groups s1 -O OpenFlow13
 sudo ovs-ofctl del-flows s2 -O OpenFlow13
 sudo ovs-ofctl del-flows s3 -O OpenFlow13
 sudo ovs-ofctl del-flows s4 -O OpenFlow13
 
-sudo ovs-ofctl -O OpenFlow13 add-group s1 group_id=1,type=select,bucket=bucket=output:2,bucket=output:3 
+# for port-MAC mapping
+for i in `seq 1 10`;
+do
+    #s0   port->MAC
+    udp_port=$(($i+5000))
+    dl_dst="00:00:00:00:"$(printf "%02d" $((udp_port/100)))":"$(printf "%02d" $((udp_port%100)))
+
+    sudo ovs-ofctl -O OpenFlow13 add-flow s0 priority=10,in_port=1,ip,udp,tp_dst=$udp_port,actions=mod_dl_dst:$dl_dst,output:2
+    
+done
+
+sudo ovs-ofctl -O OpenFlow13 add-flow s4 priority=0,in_port=1,actions=mod_dl_dst="00:00:00:00:00:02",output:3
+sudo ovs-ofctl -O OpenFlow13 add-flow s4 priority=0,in_port=2,actions=mod_dl_dst="00:00:00:00:00:02",output:3
+sudo ovs-ofctl -O OpenFlow13 add-flow s0 priority=0,in_port=1,actions=output:2
+sudo ovs-ofctl -O OpenFlow13 add-flow s0 priority=0,in_port=2,actions=output:1
+
+sudo ovs-ofctl dump-flows s0 -O OpenFlow13
+sudo ovs-ofctl dump-flows s4 -O OpenFlow13
+
+# for h0-h1-h2 (hash)
+sudo ovs-ofctl -O OpenFlow13 add-group s1 group_id=1,type=select,bucket=output:2,bucket=output:3 
 sudo ovs-ofctl add-flow s1 priority=0,in_port=1,actions=group:1 -O OpenFlow13
 
 sudo ovs-ofctl add-flow s2 priority=0,in_port=1,actions=output:2 -O OpenFlow13
@@ -16,3 +33,11 @@ sudo ovs-ofctl add-flow s3 priority=0,in_port=1,actions=output:2 -O OpenFlow13
 
 sudo ovs-ofctl add-flow s4 priority=0,in_port=1,actions=output:3 -O OpenFlow13
 sudo ovs-ofctl add-flow s4 priority=0,in_port=2,actions=output:3 -O OpenFlow13
+
+# for h2-h1 (ARP)
+sudo ovs-ofctl add-flow s1 priority=0,in_port=2,actions=output:1 -O OpenFlow13
+sudo ovs-ofctl add-flow s2 priority=0,in_port=2,actions=output:1 -O OpenFlow13
+sudo ovs-ofctl add-flow s4 priority=0,in_port=3,actions=output:1 -O OpenFlow13
+
+sudo ovs-ofctl dump-flows s1 -O OpenFlow13
+sudo ovs-ofctl dump-groups s1 -O OpenFlow13
