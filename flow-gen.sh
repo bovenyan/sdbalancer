8bd52638441963823cf1947462846c374f34f642
Boven <boven.yan@gmail.com>
2017-11-27 18:05:37 -0500

    compile past

--- flow-gen.sh
+++ flow-gen.sh
@@ -2,7 +2,7 @@
 
 for i in `seq 1 $1`;    # generate <flow number> udp flows to, port range 5001 ~ 5000+<flow number>
 do
-    iperf -c 10.0.0.2 -p $(($i+5000)) -u -b 10m -l 1000 &
+    iperf -c 10.0.0.2 -p $(($i+5000)) -u -b 1m -t 20 -l 1000 &
 done
 
 
